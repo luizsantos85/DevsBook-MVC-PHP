70145729000e4b0a7a8117d482b529b5b638a364
Luiz Henrique Santos <luiz.santos85@gmail.com>
2021-03-16 06:31:22 -0300

    Ajuste do erro ao logar
    o erro estava na função verifyLogin.

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -11,13 +11,12 @@ class LoginHandler
     if (!empty($_SESSION['token'])) {
       $token = $_SESSION['token'];
       $data = User::select()->where('token', $token)->one();
-      // $data = User::select()->where('token', $token)->execute();
 
       if (count($data) > 0) {
         $loggedUser = new User();
-        $loggedUser->id = $data['id'];
-        $loggedUser->name = $data['name'];
-        $loggedUser->email = $data['email'];
+        $loggedUser->id = $data["id"];
+        $loggedUser->name = $data["name"];
+        $loggedUser->email = $data["email"];
 
         return $loggedUser;
       }
@@ -28,12 +27,11 @@ class LoginHandler
   public static function verifyLogin($email, $password)
   {
     $user = User::select()->where('email', $email)->one();
+
     if ($user) {
       if (password_verify($password, $user['password'])) {
         $token = md5(time() . rand(0, 9999) . time());
-
-        User::update()->set('token', $token)->where('email,$email')->execute();
-
+        User::update()->set('token', $token)->where('email', $email)->execute();
         return $token;
       }
     }
