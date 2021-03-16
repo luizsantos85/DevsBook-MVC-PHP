ad69912b69e67976f1b1a50668cb1569adcabff2
Luiz Henrique Santos <luiz.santos85@gmail.com>
2021-03-16 09:48:36 -0300

    views ajustes
    ajuste do header, footer e sidebar

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -5,7 +5,36 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
   <link rel="stylesheet" href="<?= $base; ?>/assets/css/style.css" />
-  <title></title>
+  <title>Home - DevsBook</title>
 </head>
 
 <body>
+
+
+  <header>
+    <div class="container">
+      <div class="logo">
+        <a href=""><img src="<?= $base; ?>/assets/images/devsbook_logo.png" /></a>
+      </div>
+      <div class="head-side">
+        <div class="head-side-left">
+          <div class="search-area">
+            <form method="GET">
+              <input type="search" placeholder="Pesquisar" name="s" />
+            </form>
+          </div>
+        </div>
+        <div class="head-side-right">
+          <a href="" class="user-area">
+            <div class="user-area-text">Bonieky</div>
+            <div class="user-area-icon">
+              <img src="<?= $base; ?>/media/avatars/avatar.jpg" />
+            </div>
+          </a>
+          <a href="" class="user-logout">
+            <img src="<?= $base; ?>/assets/images/power_white.png" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
